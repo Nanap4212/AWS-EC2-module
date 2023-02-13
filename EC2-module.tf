196a20b6cc7f365db9dcc03e8fb1a21e71145136
Nanap4212 <nanap5625@gmail.com>
2023-02-13 23:54:34 +0530

    Combine 2 in 1
    changes for merge
    
    commit1
    
    commit2

--- EC2-module.tf
+++ EC2-module.tf
@@ -2,8 +2,11 @@ resource "aws_instance" "New" {
     ami = "123"
     instance = "t2.micro"
     security_groups = "default"
-    vpc_security_group_ids = "vpc12345"
     count = 2
     key_name = "allinone.pem"
+    vpc_security_group_ids = "vpcid"
+    tags = {
+      name = ""
+    }
     
 }
