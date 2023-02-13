db188bd1a98aaf6903c72414804aeecd4e81f5f7
Nanap4212 <nanap5625@gmail.com>
2023-02-13 23:22:39 +0530

    changes for merge

--- EC2-module.tf
+++ EC2-module.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "New" {
     ami = "123"
     instance = "t2.micro"
     security_groups = "default"
-    vpc_security_group_ids = "vpc"
+    vpc_security_group_ids = "vpc12345"
+    count = 2
+    key_name = "allinone.pem"
     
 }
