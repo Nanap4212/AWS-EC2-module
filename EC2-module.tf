22b48ad4fda17e8d87e2ae4d726f6a9d45e33762
Nanap4212 <nanap5625@gmail.com>
2023-02-13 23:25:22 +0530

    Revert "changes for merge"
    This reverts commit db188bd1a98aaf6903c72414804aeecd4e81f5f7.

--- EC2-module.tf
+++ EC2-module.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "New" {
     ami = "123"
     instance = "t2.micro"
     security_groups = "default"
-    vpc_security_group_ids = "vpc12345"
-    count = 2
-    key_name = "allinone.pem"
+    vpc_security_group_ids = "vpc"
     
 }
