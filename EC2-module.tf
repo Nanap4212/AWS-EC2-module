6ef959e2b129fe8dee3f6020096e0c7fc5a462ee
Nanap4212 <nanap5625@gmail.com>
2023-02-13 23:29:16 +0530

    i

--- EC2-module.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "New" {
-    ami = "123"
-    instance = "t2.micro"
-    security_groups = "default"
-    vpc_security_group_ids = "vpc12345"
-    count = 2
-    key_name = "allinone.pem"
-    
-}
