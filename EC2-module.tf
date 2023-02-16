aed40652753b5ffb7047646d43787d149fb1c012
Nanap4212 <nanap5625@gmail.com>
2023-02-16 19:30:54 +0530

    ec2 module

--- EC2-module.tf
+++ EC2-module.tf
@@ -1,12 +1,23 @@
-resource "aws_instance" "New" {
-    ami = "123"
-    instance = "t2.micro"
-    security_groups = "default"
-    count = 2
-    key_name = "allinone.pem"
-    vpc_security_group_ids = "vpcid"
-    tags = {
-      name = ""
-    }
-    
+resource "aws_instance" "instanace1" {
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = "open"
+
+  tags = {
+    name = "newEC2"
+  }
+}
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "key-pair" {
+  key_name   = "open"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
 }
